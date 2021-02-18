b4ab50f46fd3e2871b39b7aa12d3442c2238557e
lj1181125 <2319064595@qq.com>
2021-02-18 15:59:10 +0800

    增加定时任务代码

---
+++ high_learning_record/src/main/java/lijie/test/quartzTest/PrintWordsJob.java
@@ -0,0 +1,23 @@
+package lijie.test.quartzTest;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Random;
+
+/**
+ * Created by wanggenshen
+ * Date: on 2018/7/7 16:28.
+ * Description: 打印任意内容
+ */
+public class PrintWordsJob implements Job {
+
+    public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String printTime = new SimpleDateFormat("yy-MM-dd HH-mm-ss").format(new Date());
+        System.out.println("PrintWordsJob start at:" + printTime + ", prints: Hello Job-" + new Random().nextInt(100));
+
+    }
+}
